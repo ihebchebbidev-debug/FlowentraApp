2e4089f39fd28dcf014b279427ec96f33da3228f
ihebchebbi1998tn <iheb@spadadibattaglia.com>
2026-06-24 14:33:06 +0100

    ad

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import PasswordGate from './components/PasswordGate';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PasswordGate>
+      <App />
+    </PasswordGate>
   </StrictMode>,
 )
 
